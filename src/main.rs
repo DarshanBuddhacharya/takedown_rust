acd08c755526cbe1f7e88dbb42306f3a80667ef2
Darshan <np03a190140@heraldcollege.edu.np>
2023-03-27 21:38:24 +0545

    feat(rect-move) rectangle player movement added

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ mod resources;
 
 #[macroquad::main("Takedown")]
 async fn main() {
-    let player_1 = Player::new(0.02);
-    let player_2 = Player::new(0.92);
+    let mut player_1 = Player::new(0.02);
+    let mut player_2 = Player::new(0.92);
     loop {
+        player_1.update(get_frame_time(), true);
+        player_2.update(get_frame_time(), false);
         clear_background(BLACK);
         player_1.draw(BLUE);
         player_2.draw(GREEN);
