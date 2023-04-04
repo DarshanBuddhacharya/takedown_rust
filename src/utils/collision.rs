583a8b298891958c0c870e5f8fc808707607df18
Darshan <np03a190140@heraldcollege.edu.np>
2023-04-04 22:50:49 +0545

    feat(fire) fire texture added

--- src/utils/collision.rs
+++ src/utils/collision.rs
@@ -1,34 +1,37 @@
-use macroquad::prelude::{Rect, Vec2};
-
 use macroquad::prelude::*;
+use macroquad::prelude::{Rect, Vec2};
+use macroquad_particles::Emitter;
 
 //This function detects ball collision with respect to the player and changes the ball's velocity to opposite(In different direction)
-pub fn detect_collision(circle: &mut Circle, vel: &mut Vec2, rect: &Rect) -> bool {
+pub fn detect_collision(
+    circle: &mut Circle,
+    vel: &mut Vec2,
+    rect: &Rect,
+    emmitor: &mut Emitter,
+) -> bool {
     let intersection = circle.overlaps_rect(&*rect);
 
     let center = Rect::new(rect.x, rect.center().y - 20.0 * 0.5, 40.0, 30.0);
 
     let center_intersection = circle.overlaps_rect(&*&center);
 
-    if intersection {
-        // if center_intersection {
-        //     debug!("oh yess daddy");
-        //     vel.x *= -1.0;
-        // }
-        // debug!("-----------");
+    emmitor.draw(vec2(circle.x, circle.y));
 
+    if intersection {
         if circle.point().x > rect.point().x {
             if center_intersection {
-                debug!("-----------");
                 vel.x *= -1.5;
+                emmitor.config.emitting = true
             } else {
+                emmitor.config.emitting = false;
                 vel.x = 1.0;
             }
         } else if circle.point().x < rect.point().x {
             if center_intersection {
-                debug!("-----------");
                 vel.x *= -1.5;
+                emmitor.config.emitting = true
             } else {
+                emmitor.config.emitting = false;
                 vel.x = -1.0;
             }
         }
